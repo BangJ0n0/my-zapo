429921704fdf9eb07870ec3d21b1f0fc786ca644
BangJ0n0 <bangjono546@gmail.com>
2026-09-08 19:07:27 +0700

    Upload files

---
+++ index.js
@@ -0,0 +1,64 @@
+// index.js
+
+import chalk from 'chalk'
+import { createSocket } from './src/createSocket.js'
+import { connectionHandler } from './src/connectionHandler.js'
+import { messageHandler } from './src/messageHandler.js'
+import { groupEventHandler } from './src/groupEventHandler.js'
+import { startThumbAutoRefresh } from './lib/thumbAutoRefresh.js'
+
+let sock
+
+try {
+  sock = createSocket()
+
+  connectionHandler(sock)
+  groupEventHandler(sock)
+
+  messageHandler(sock).catch(err => {
+    console.error(
+      chalk.red('[MESSAGE] Handler error:'),
+      err?.stack || err?.message || err
+    )
+  })
+
+  await sock.connect()
+
+  startThumbAutoRefresh(sock)
+
+} catch (err) {
+  console.error(
+    chalk.red('[WA] Gagal connect awal:'),
+    err?.stack || err?.message || err
+  )
+
+  process.exit(1)
+}
+
+let shuttingDown = false
+
+async function shutdown(signal) {
+  if (shuttingDown) return
+
+  shuttingDown = true
+
+  console.log(
+    chalk.yellow(`\n[WA] Menerima ${signal}, menutup koneksi...`)
+  )
+
+  try {
+    if (sock) {
+      await sock.disconnect()
+    }
+  } catch (err) {
+    console.error(
+      chalk.red('[WA] Gagal menutup koneksi:'),
+      err?.stack || err?.message || err
+    )
+  }
+
+  process.exit(0)
+}
+
+process.on('SIGINT', () => shutdown('SIGINT'))
+process.on('SIGTERM', () => shutdown('SIGTERM'))
